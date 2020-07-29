2d07e228e26d6b41fa3311f3e210db794cfecd7d
askirmas <kirmas@phystech.edu>
2020-07-30 01:18:35 +0300

    Fix cy.sh falsy comparisson

--- cypress/cy.sh
+++ cypress/cy.sh
@@ -6,13 +6,13 @@
 
 OPTS=()
 
-if [ ${npm_config_run:-0} == 0 ];
+if [ -z "$npm_config_run" ] || [ "$npm_config_run" == "0" ];
 then
   OPTS+=('open')
 else
   OPTS+=('run' '--browser' "$npm_config_run")
 
-  if [ ${npm_config_hed:-0} == 0 ]
+  if [ -z "$npm_config_hed" ] || [ "$npm_config_hed" == "0" ]
   then 
     head="--headless"
   else
@@ -21,7 +21,7 @@ else
 
   OPTS+=("$head")
 
-  if [ ${npm_config_spec:-0} -ne 0 ]; then
+  if [ ! -z "$npm_config_spec" ] && [ "$npm_config_spec" != "0" ]; then
     OPTS+=('--spec' "cypress/integration/$npm_config_spec.*")
   fi
 fi 
@@ -29,7 +29,7 @@ fi
 #TODO $npm_config_config_file
 OPTS+=('--config-file' 'node_modules/@kirmas/template-cypress/config.json')
 
-if [ ${npm_config_dev:-0} -ne 0 ]; then
+if [ ! -z "$npm_config_dev" ] && [ "$npm_config_dev" != "0" ]; then
   OPTS+=('--config' "baseUrl=http://localhost:${PORT:-3000}/")
 fi
 
