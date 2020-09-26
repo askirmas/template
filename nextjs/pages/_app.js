6ab21ef8b158711fe06d97328392162312cd057c
Andrii Kirmas <kirmas@phystech.edu>
2020-09-26 14:43:03 +0300

    Add default responsive meta tags

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,8 +1,19 @@
 // import App from 'next/app'
+import Head from "next/head"
 
 //@ts-ignore
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta charSet="utf-8"/>
+      <meta
+        name="viewport"
+        // eslint-disable-next-line max-len
+        content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi"
+      />      
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 // Only uncomment this method if you have blocking data requirements for
